617822c52d0d80f841c545c9eec19c5fc8d66684
Alex Baranowski <alex@euro-linux.com>
2020-01-10 02:26:10 +0100

    els-ASAP auto commit file/tag gnome-keyring-3.8.2-8.el7.src.rpm

---
+++ src/pkcs11/xdg-store/gkm-xdg-asn1-defs.h
@@ -0,0 +1,31 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 8; tab-width: 8 -*- */
+/* egg-asn1-defs.h - ASN.1 definitions
+
+   Copyright (C) 2010 Stefan Walter
+
+   The Gnome Keyring Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   The Gnome Keyring Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with the Gnome Library; see the file COPYING.LIB.  If not,
+   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+
+   Author: Stef Walter <stef@memberwebs.com>
+*/
+
+#ifndef GKM_XDG_ASN1_DEFS_H_
+#define GKM_XDG_ASN1_DEFS_H_
+
+#include "egg/egg-asn1-defs.h"
+
+extern const struct _EggAsn1xDef xdg_asn1_tab[];
+
+#endif /* GKM_XDG_ASN1_DEFS_H_ */
